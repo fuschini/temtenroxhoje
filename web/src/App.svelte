c384b65ac83a485d8117253992f47621c80416cb
Henrique Fuschini <henriquefuschini@gmail.com>
2024-01-14 18:38:41 -0600

    adjust mobile layout to fit on screen

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -21,21 +21,23 @@
 </script>
 
 <main
-    class="w-[100vw] h-[100vh] flex justify-center items-center flex-col {reminder
+    class="h-[100%] flex justify-center items-center flex-col {reminder
         ? 'bg-green-600'
         : 'bg-red-600'}"
 >
-    <div class="text-white text-xl">Tem Tenrox hoje?</div>
+    <div class="text-white text-xl font-mono">Tem Tenrox hoje?</div>
 
     <div class="text-[10em] font-bold flex justify-center">
         {reminder ? "Sim" : "Não"}
     </div>
 
-    <div class="flex justify-center">
+    <div class="flex justify-center text-xl">
         {reminder ? reminder.message : ""}
     </div>
 
-    <div class="absolute bottom-2 w-[100%] text-center flex justify-center items-baseline gap-1">
+    <div
+        class="absolute bottom-2 w-[100%] text-center flex justify-center items-baseline gap-1 font-mono"
+    >
         <span>Made with</span>
         <img src="/squirrel.png" alt="Squirrel disguised as detective emoji" class="w-6" />
         <span>by Fusca</span>
